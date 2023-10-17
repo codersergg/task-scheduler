d2cd09f5189777b0ccee15ce64f420623afaa099
Sergey Shopen <sergey.i.chopin@gmail.com>
2023-10-17 16:17:40 +0200

    rename properties

--- src/main/kotlin/com/codersergg/taskscheduler/configuration/ApplicationProperties.kt
+++ src/main/kotlin/com/codersergg/taskscheduler/configuration/ApplicationProperties.kt
@@ -2,5 +2,5 @@ package com.codersergg.taskscheduler.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "task-scheduler.deployment-uuid")
+@ConfigurationProperties(prefix = "task-scheduler.app-initializer")
 data class ApplicationProperties(val updateTime: String = "")
